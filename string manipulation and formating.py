d43cc0bd5c2d78f5546a60198c846fa5c660e797
Anvay75 <anvaz2880@gmail.com>
2025-02-26 16:52:33 +0100

    Add files via upload

---
+++ string manipulation and formating.py
@@ -0,0 +1,25 @@
+#string manipulation
+x = "     nobody in the planet is dumb       "
+print(x.strip())
+#letter uppercase / lowercase
+x="nobody in the planet is dumb"
+print(x.upper())
+x="NOBODY IN THE PLANET IS DUMB"
+print(x.lower())
+#making sure the list works correctly
+x=("nobody in the planet is very dumb")
+print(x.split())
+
+
+#string formating 
+name = "Anvay"
+age = "10"
+print("{} is {} years old".format(name,age))
+
+# another way to learn buy planelearn
+print(name + " is " + str(age) + " years old")
+
+#slicing apart a string as if it was a list
+a = "apple , bannana , mango , cherry , framboose"
+print(a[:])
+print(a [0::2])
